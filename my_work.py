f0b007de688ba6f9cf3fa92fef52f9af625d5b46
Aliabdulaev <ali.abdulaev87@yandex.ru>
2021-12-09 11:17:05 +0300

    ДЗ 4

---
+++ my_work.py
@@ -0,0 +1,7 @@
+def my_sum(a: int, b: int):
+    return a + b
+
+def my_div(a: int, b: int):
+    return a / b
+def input(a):
+    print('Хотел ввести данные АХАХАХАХА Больше такой возможности у тебя нет')
